c7a94ce2a752afa7652ae61aea22cdaeafe5c42b
Adam Balogh <balogh.adam@icloud.com>
2016-08-10 22:14:42 +0100

    parse string handles escape chars

--- json_parser.cc
+++ json_parser.cc
@@ -14,6 +14,7 @@ const char kDoubleQuote = '"';
 const char kWhitespace = ' ';
 const char kColon = ':';
 const char kComma = ',';
+const char kEscapeChar = '\\';
 
 void JsonParser::Parse(const char* p, const char* end) { ParseObject(p, end); }
 
@@ -37,7 +38,7 @@ const char* JsonParser::ParseObject(const char* p, const char* end) {
     return ++p;
   }
 
-  while (p != end) {
+  while (true) {
     p = ParseString(p, end);
     assert(*p == kColon);
     ++p;
@@ -53,21 +54,14 @@ const char* JsonParser::ParseObject(const char* p, const char* end) {
   return ++p;
 }
 
-const char* JsonParser::ParseString(const char* p, const char* end) {
-  assert(*p == kStringOpen);
-  ++p;
-  auto stringEnd = Find(p, end, kStringClose);
-  std::cout << "string: " << std::string{p, stringEnd} << std::endl;
-  return ++stringEnd;
-}
-
 const char* JsonParser::ParseArray(const char* p, const char* end) {
   assert(*p == kArrayOpen);
   ++p;
   if (*p == kArrayClose) {
     return ++p;
   }
-  while (p != end) {
+
+  while (true) {
     p = ParseValue(p, end);
     if (*p == kArrayClose) {
       break;
@@ -75,14 +69,30 @@ const char* JsonParser::ParseArray(const char* p, const char* end) {
     assert(*p == kComma);
     ++p;
   }
+
   assert(*p == kArrayClose);
   return ++p;
 }
 
-const char* JsonParser::ParseNumber(const char* p, const char* end) {
+const char* JsonParser::ParseString(const char* p, const char* end) {
+  assert(*p == kStringOpen);
+  ++p;
+  const char* const string_start = p;
+  while (true) {
+    p = Find(p, end, kStringClose);
+    if (*(p - 1) != kEscapeChar) {
+      break;
+    }
+    ++p;
+  }
+  std::cout << "string: " << std::string{string_start, p} << std::endl;
+  return ++p;
+}
+
+const char* JsonParser::ParseNumber(const char* p, const char* const end) {
   int num = 0;
   assert(std::isdigit(*p));
-  while (p != end && std::isdigit(*p)) {
+  while (std::isdigit(*p)) {
     num *= 10;
     num += *p - '0';
     ++p;
@@ -92,23 +102,15 @@ const char* JsonParser::ParseNumber(const char* p, const char* end) {
 }
 
 const char* JsonParser::Find(const char* p, const char* end, const char val) {
-  while (p != end && *p != val) {
+  while (*p != val) {
     ++p;
   }
   return p;
 }
 
 const char* JsonParser::SkipWhitespace(const char* p, const char* end) {
-  while (p != end && *p == kWhitespace) {
+  while (*p == kWhitespace) {
     ++p;
   }
   return p;
-}
-
-int main() {
-  std::string test =
-      "{\"name\":\"Carl\",\"age\":10,\"food\":[\"spaghetti\",\"ice-cream\"],"
-      "\"empty\":{},\"sub\":{\"fake\":10}}";
-  JsonParser p;
-  p.Parse(&test[0], &test[test.size()]);
 }
--- json_parser.h
+++ json_parser.h
@@ -2,16 +2,23 @@
 
 #include <string>
 
+// Json parser using specification from http://json.org/
+//
+// Current implementation assumes that the given string's syntax is correct, and
+// it only prints the values it finds. It doesn't tolerate any whitespace
+// either.
+//
 class JsonParser {
  public:
   void Parse(const char* p, const char* end);
+  void Parse(const std::string& json) { Parse(&json[0], &json[json.size()]); }
 
  private:
   const char* ParseValue(const char* p, const char* const end);
   const char* ParseObject(const char* p, const char* const end);
   const char* ParseArray(const char* p, const char* const end);
   const char* ParseString(const char* p, const char* const end);
-  const char* ParseNumber(const char* p, const char* end);
+  const char* ParseNumber(const char* p, const char* const end);
 
   const char* SkipWhitespace(const char* p, const char* const end);
   const char* Find(const char* p, const char* const end, const char val);
