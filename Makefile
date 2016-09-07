e5753fb451cfab53ea6627d9f04a825aec3e3b9a
Adam Balogh <balogh.adam@icloud.com>
2016-09-07 23:39:41 +0100

    updated benchmarks

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ test: src/json_parser_test.cc src/json_parser.cc src/json_parser.h src/json_valu
 	clang++ -std=c++14 src/json_parser.cc src/json_parser_test.cc -lgtest -lboost_system-mt -lboost_filesystem-mt -o json_parser_test -Wall -Werror
 
 benchmark_main: benchmark/main.cc src/json_parser.cc src/json_parser.h src/json_value.h
-	clang++ -std=c++14 -O3 -DNDEBUG benchmark/main.cc src/json_parser.cc -lbenchmark -o benchmark_main
+	clang++ -std=c++14 -O3 -DNDEBUG benchmark/main.cc src/json_parser.cc -lbenchmark -lboost_system-mt -lboost_thread-mt -lboost_chrono-mt -lboost_date_time-mt -lcpprest -ljsoncpp -o benchmark_main
 
 clean:
 	rm benchmark_main json_parser_test
--- benchmark/main.cc
+++ benchmark/main.cc
@@ -5,7 +5,8 @@
 
 #include "../src/json_parser.h"
 #include "nlohmann/json.hpp"
-#include "rapidjson/document.h"
+#include "cpprest/json.h"
+#include "json/json.h"
 
 static std::ifstream file("test_data/benchmark/twitter.json");
 static const std::string e((std::istreambuf_iterator<char>(file)),
@@ -23,15 +24,35 @@ static void nlohmannParse(benchmark::State& state) {
   }
 }
 
-static void rapidjsonParse(benchmark::State& state) {
+static void cppRestParse(benchmark::State& state) {
   while (state.KeepRunning()) {
-    rapidjson::Document d;
-    d.Parse(e.c_str());
+    web::json::value::parse(e);
+  }
+}
+
+static void jsonCppParse(benchmark::State& state) {
+  Json::Reader reader;
+  Json::Value value;
+  while (state.KeepRunning()) {
+    reader.parse(e, value, false);
   }
 }
 
 BENCHMARK(jpParse);
 BENCHMARK(nlohmannParse);
-BENCHMARK(rapidjsonParse);
+BENCHMARK(cppRestParse);
+BENCHMARK(jsonCppParse);
 
+/* Results:
+ *
+ * Run on (8 X 1000 MHz CPU s)
+ * 2016-09-07 23:38:11
+ * Benchmark       Time(ns)    CPU(ns) Iterations
+ * ----------------------------------------------
+ * jpParse          8256589    8199881         84
+ * nlohmannParse   15699699   15655044         45
+ * cppRestParse     7834885    7831289         83
+ * jsonCppParse    13401031   13382741         54
+ *
+ */
 BENCHMARK_MAIN();
