3a59d605311c3b2e92f3e6428508cbe27062edb5
Adam Balogh <balogh.adam@icloud.com>
2016-09-07 23:07:36 +0100

    updated tests

--- src/json_parser_test.cc
+++ src/json_parser_test.cc
@@ -18,7 +18,7 @@ TEST(JsonParser, EmptyJson) {
   EXPECT_EQ(0, obj.size());
 }
 
-TEST(JsonParser, GetString) {
+TEST(JsonParser, String) {
   string e = "{\"name\":\"Adam\"}";
   auto obj = JsonParser{e}.Parse();
   EXPECT_TRUE(obj.getObject().at("name").is<JsonValue::STRING>());
@@ -32,6 +32,25 @@ TEST(JsonParser, Bool) {
   EXPECT_EQ(true, obj.getObject().at("val").getBool());
 }
 
+TEST(JsonParser, NumberParsing) {
+  std::vector<std::pair<std::string, double>> tests{{"-1", -1},
+                                                    {"1", 1},
+                                                    {"0", 0},
+                                                    {"0.1", 0.1},
+                                                    {"1e0", 1},
+                                                    {"1.4e2", 140},
+                                                    {"10e3", 10000},
+                                                    {"10e+3", 10000},
+                                                    {"10E+3", 10000},
+                                                    {"-0.12e3", -120},
+                                                    {"1e-2", 0.01}};
+  for (const auto& pair : tests) {
+    std::string json = "{\"num\": " + pair.first + "}";
+    auto obj = JsonParser{json}.Parse().getObject();
+    EXPECT_EQ(obj.at("num").getNumber(), pair.second);
+  }
+}
+
 TEST(JsonParser, EmptyArray) {
   string e = "{\"name\":[]}";
   auto obj = JsonParser{e}.Parse();
@@ -39,7 +58,47 @@ TEST(JsonParser, EmptyArray) {
   EXPECT_EQ(0, obj.getObject().at("name").getArray().size());
 }
 
-TEST(JsonParser, Complex) {
+TEST(JsonParser, Array) {
+  string e = "{\"name\":[\"a\",\"b\"]}";
+  auto obj = JsonParser{e}.Parse();
+  EXPECT_TRUE(obj.getObject().at("name").is<JsonValue::ARRAY>());
+  auto arr = obj.getObject().at("name").getArray();
+  EXPECT_EQ(2, arr.size());
+  EXPECT_EQ("a", arr[0].getString());
+  EXPECT_EQ("b", arr[1].getString());
+}
+
+TEST(JsonParser, InvalidBool) {
+  string e = "{\"val\": trio}";
+  EXPECT_THROW(JsonParser{e}.Parse(), std::runtime_error);
+}
+
+TEST(JsonParser, InvalidJson) {
+  std::vector<std::string> jsons{
+      ",", "{\"a\":[ :}", "{\"num\": 10, }", "{\"name\" }", "{\"name....}",
+      "{\"name\"   \"joe\"}", "{\"Illegal expression\": 1 + 2}"};
+  for (const auto& a : jsons) {
+    try {
+      JsonParser{a}.Parse();
+      FAIL();
+    } catch (std::exception& e) {
+    }
+  }
+}
+
+TEST(JsonParser, InvalidNumber) {
+  std::vector<std::string> jsons = {"{\"a\": 1", "{\"a\": 1.", "{\"a\": 1.}"};
+
+  for (const auto& json : jsons) {
+    try {
+      JsonParser{json}.Parse();
+      FAIL();
+    } catch (std::exception& e) {
+    }
+  }
+}
+
+TEST(JsonParser, ComplexJson) {
   string e =
       "{\"name\":\"Carl\",\"age\":-0.010,\"food\":[\"spaghetti\",\"ice-"
       "cream\"],\"sub\":{\"fake\":-10.94}}";
@@ -60,16 +119,11 @@ TEST(JsonParser, StringEscape) {
   }
 }
 
-TEST(JsonParser, HandlesWhiteSpace) {
+TEST(JsonParser, SkipWhiteSpace) {
   string e = " { \" na me \"   :  \" A da m \"    , \"age   \":  10  }";
   auto obj = JsonParser{e}.Parse().getObject();
   EXPECT_EQ(" A da m ", obj.at(" na me ").getString());
   EXPECT_EQ(10, obj.at("age   ").getNumber());
-}
-
-TEST(JsonParser, InvalidBool) {
-  string e = "{\"val\": trio}";
-  EXPECT_THROW(JsonParser{e}.Parse(), std::runtime_error);
 }
 
 TEST(JsonParser, JsonOrgTests) {
@@ -93,50 +147,6 @@ TEST(JsonParser, JsonFailTests) {
     try {
       JsonParser{e}.Parse();
       std::cout << file_entry.path() << std::endl;
-      FAIL();
-    } catch (std::exception& e) {
-    }
-  }
-}
-
-TEST(JsonParser, InvalidJson) {
-  std::vector<std::string> jsons{
-      ",", "{\"a\":[ :}", "{\"num\": 10, }", "{\"name\" }", "{\"name....}",
-      "{\"name\"   \"joe\"}", "{\"Illegal expression\": 1 + 2}"};
-  for (const auto& a : jsons) {
-    try {
-      JsonParser{a}.Parse();
-      FAIL();
-    } catch (std::exception& e) {
-    }
-  }
-}
-
-TEST(JsonParser, NumberParsing) {
-  std::vector<std::pair<std::string, double>> tests{{"-1", -1},
-                                                    {"1", 1},
-                                                    {"0", 0},
-                                                    {"0.1", 0.1},
-                                                    {"1e0", 1},
-                                                    {"1.4e2", 140},
-                                                    {"10e3", 10000},
-                                                    {"10e+3", 10000},
-                                                    {"10E+3", 10000},
-                                                    {"-0.12e3", -120},
-                                                    {"1e-2", 0.01}};
-  for (const auto& pair : tests) {
-    std::string json = "{\"num\": " + pair.first + "}";
-    auto obj = JsonParser{json}.Parse().getObject();
-    EXPECT_EQ(obj.at("num").getNumber(), pair.second);
-  }
-}
-
-TEST(JsonParser, InvalidNumberParsing) {
-  std::vector<std::string> jsons = {"{\"a\": 1", "{\"a\": 1."};
-
-  for (const auto& json : jsons) {
-    try {
-      JsonParser{json}.Parse();
       FAIL();
     } catch (std::exception& e) {
     }
